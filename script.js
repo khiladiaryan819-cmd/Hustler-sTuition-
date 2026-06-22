4a86ce2dde41926469394e743e4b07aad6c666dd
khiladiaryan819-cmd <khiladiaryan819@gmail.com>
2026-06-22 15:31:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -476,3 +476,9 @@ window.addEventListener("scroll", () => {
     }
 
 });
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
